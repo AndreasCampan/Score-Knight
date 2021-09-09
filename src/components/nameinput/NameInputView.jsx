587ac77daec66e2fab4712948e25ac1dbc187061
Andreas <andreascampan@gmail.com>
2021-09-09 15:38:26 -0600

    -Added message logic based on user input -styled the layout of the input

--- src/components/nameinput/NameInputView.jsx
+++ src/components/nameinput/NameInputView.jsx
@@ -19,23 +19,30 @@ class NameInputView extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.addPlayer(this.state.nameInput);
+    this.setState({ nameInput: ''});
   }
 
   render() {
-    let messageBool = this.props.duplicateName;
+    let messageNum = this.props.duplicateName;
     let dupMessage;
 
-    if (!messageBool) {
+    if (messageNum === 0) {
       dupMessage = <div className="duplicate-name"></div>
+    } else if (messageNum === 1) {
+      dupMessage = <div className="duplicate-name">The player already exists!</div>
     } else {
-      dupMessage = <div className="duplicate-name">This name already exists, please select a different name</div>
+      dupMessage = <div className="duplicate-name">Please enter a name!</div>
     }
+
     return(
       <div className="nameInput-container">
-        <form name="Score" onSubmit={this.handleSubmit}>
-          <input type="text" onChange={this.onInput} id="nameinput" placeholder="Type a Name..." />
-          <button type='submit'>Submit</button>
-        </form>
+        <div className="input-inner-box">
+          <h2 className="add-players-title">Add Players:</h2>
+          <form name="Score" onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.onInput} value={this.state.nameInput} id="nameinput" placeholder="Type a Name..." />
+            <button className="name-sub" type='submit'>Submit</button>
+          </form>
+        </div>
         { dupMessage }
       </div>
     )
