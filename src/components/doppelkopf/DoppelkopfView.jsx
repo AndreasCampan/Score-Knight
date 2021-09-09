6dcbe4947f7014888c22d0a5eaf60bd13ecaf64a
Andreas <andreascampan@gmail.com>
2021-09-09 10:33:00 -0600

    Modfifed the naming of Doppelkopf and added content to the view

---
+++ src/components/doppelkopf/DoppelkopfView.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Doppelkopf.css'
+
+class DoppelkopfView extends React.Component {
+
+  render(){
+    return(
+      <div className="basic-container">
+        <h1>Doppelkopf</h1>
+      </div>
+    )
+  }
+}
+
+export default DoppelkopfView;
