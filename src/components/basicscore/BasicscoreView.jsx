ddff904f8daa21bac02b98d4f94a1176c79ad2a6
Andreas <andreascampan@gmail.com>
2021-09-10 11:46:56 -0600

    -designed the layout of the basic score keeper -Added a total scorebaord -Added +/-1 and +/-5 buttons to player cards

--- src/components/basicscore/BasicscoreView.jsx
+++ src/components/basicscore/BasicscoreView.jsx
@@ -7,13 +7,37 @@ class BasicScoreView extends React.Component {
     let playersArray = this.props.players
     let drawPlayers = playersArray.map((data) => {
       return (
-      <li key={data.id}>{data.name}</li>
+      <li className="player-card" key={data.id}>
+        {data.name}
+        <span className="player-score">0</span>
+        <div className="bttn-container">
+          <button className="basic-bttn-minus up">-5</button>
+          <button className="basic-bttn-minus down">-1</button>
+          <button className="basic-bttn-plus down">+1</button>
+          <button className="basic-bttn-plus up">+5</button>
+        </div>
+      </li>
+      )
+    });
+
+    let scoreTable = playersArray.map((data) => {
+      return (
+      <li className="st-player" key={data.id}>
+        {data.name}
+        
+      </li>
       )
     });
 
     return(
       <div className="basic-container">
-        <ul>
+        <div className="scoreboard">
+          <h2 className="scoreboard-title">Scoreboard</h2>
+          <ol className="totals-box">
+            { scoreTable }
+          </ol>
+        </div>
+        <ul className="player-list">
           { drawPlayers }
         </ul>
       </div>
