623c07fd449b91a8320f726165a24f2254b2af23
Andreas <andreascampan@gmail.com>
2021-09-16 16:59:07 -0600

    -Added SC logo to nav bar -Added animations to the navbar -Added an initial loading animation based off the SC logo

--- src/components/navigation/NavView.jsx
+++ src/components/navigation/NavView.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './navview.css'
+import logo from '../../img/sk-logo.svg'
 
 class NavView extends React.Component {
 
   render(){
     return(
       <div className="nav-container">
-        <h1 className="nav-title">Score Knight</h1>
+        <h1 className="nav-title">Score</h1>
+        <img src={logo} alt="Logo for Portfolio websites" className="logo" />
+        <h1 className="nav-title2">Knight</h1>
+        <div></div>
       </div>
     )
   }
